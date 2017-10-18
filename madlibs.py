a0886bb64afc10404ed77e479003db3d7a64751a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-18 13:01:21 -0700

    started building more advanced madlib

--- madlibs.py
+++ madlibs.py
@@ -62,8 +62,31 @@ def show_madlib():
     noun = request.args.get("noun").lower()
     adjective = request.args.get("adj").lower()
 
-    return render_template("madlib.html", color=color, person=person,
-                            noun=noun, adjective=adjective)
+    adj1 = request.args.get("adj1").lower()
+    n1 = request.args.get("n1").lower()
+    pv = request.args.get("pv").lower()
+    adv1 = request.args.get("adv1").lower()
+    adj2 = request.args.get("adj2").lower()
+    n2 = request.args.get("n2").lower()
+    n3 = request.args.get("n3").lower()
+    adj3 = request.args.get("adj3").lower()
+    v1 = request.args.get("v1").lower()
+    adv2 = request.args.get("adv2").lower()
+    pv2 = request.args.get("pv2").lower()
+    adj4 = request.args.get("adj4").lower()
+
+    return render_template("madlib.html", adjective1=adj1,
+                            noun1=n1,
+                            past_verb=pv,
+                            adverb1=adv1,
+                            adjective2=adj2,
+                            noun2=n2,
+                            noun3=n3,
+                            adjective3=adj3
+                            verb1=v1,
+                            adverb2=adv2,
+                            past_verb2=pv2,
+                            adjective4=adj4)
 
 
 if __name__ == '__main__':
@@ -71,3 +94,4 @@ if __name__ == '__main__':
     # "reloads" our web app if we change the code.
 
     app.run(debug=True)
+
